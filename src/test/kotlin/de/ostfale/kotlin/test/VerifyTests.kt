7a9231bd75f4ea31828ea4ac975eab125b9b625a
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2021-01-07 07:30:43 +0100

    some more verify examples

--- src/test/kotlin/de/ostfale/kotlin/test/VerifyTests.kt
+++ src/test/kotlin/de/ostfale/kotlin/test/VerifyTests.kt
@@ -28,4 +28,34 @@ internal class VerifyTests {
             santaMock.laugh(2)
         }
     }
+
+    @Test
+    @DisplayName("Verify exact number a function has been called")
+    internal fun verifyExactNumber() {
+        // given
+        val santaMock = mockk<Santa>()
+        // when
+        every { santaMock.laugh(any()) } returns "Ho ho ho!"
+        // then
+        santaMock.laugh(1)
+        verify(exactly = 1) {
+            santaMock.laugh(1)
+        }
+    }
+
+    @Test
+    @DisplayName("Verify interval number a function has been called")
+    internal fun verifyIntervalNumber() {
+        // given
+        val santaMock = mockk<Santa>()
+        // when
+        every { santaMock.laugh(any()) } returns "Ho ho ho!"
+        // then
+        santaMock.laugh(1)
+        santaMock.laugh(2)
+        santaMock.laugh(3)
+        verify(atLeast = 2, atMost = 5) {
+            santaMock.laugh(any())
+        }
+    }
 }
