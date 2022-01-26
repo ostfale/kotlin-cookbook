9017e9d2b114de8d3f87e193d0d62d4d6a147bcc
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2022-01-26 07:47:46 +0100

    Example for functional data structure
    + head and tail function for lists

--- src/main/kotlin/de/ostfale/kotlin/books/fpkotlin/FunctionalStructures.kt
+++ src/main/kotlin/de/ostfale/kotlin/books/fpkotlin/FunctionalStructures.kt
@@ -16,5 +16,45 @@ sealed class List<out A> {
                 Cons(aa[0], of(*tail))
             }
         }
+
+        fun sum(intList: List<Int>): Int =
+            when (intList) {
+                is Nil -> 0
+                is Cons -> intList.head + sum(intList.tail)
+            }
+
+        fun product(doubleList: List<Double>): Double =
+            when (doubleList) {
+                is Nil -> 1.0
+                is Cons -> {
+                    if (doubleList.head == 0.0) 0.0
+                    else doubleList.head * product(doubleList.tail)
+                }
+            }
     }
+}
+
+// some convenience functions as extensions ex.3.1 / 3.2
+fun <A> List<A>.tail(): List<A> =
+    when (this) {
+        is Cons -> this.tail
+        is Nil -> throw IllegalStateException("Nil can't have a tail")
+    }
+
+fun <A> List<A>.setHead(x: A): List<A> =
+    when (this) {
+        is Cons -> Cons(x, this.tail)
+        is Nil -> throw IllegalStateException("Cannot replace Head of a Nil list!")
+    }
+
+fun main() {
+    // simple single linked list from varargs
+    val intList = List.of(1, 2, 3, 4, 5)
+    val doubleList = List.of(1.0, 2.5, 3.1, 4.4, 5.6)
+    println(intList)
+    println(List.sum(intList))
+    println(List.product(doubleList))
+
+    println("Extension tail before: $intList and after tail function\n: ${intList.tail()}")
+    println("Extension set Head before: $intList and after setHead function\n: ${intList.setHead(666)}")
 }
